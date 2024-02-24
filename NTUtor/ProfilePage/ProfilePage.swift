160f78cf54a5874c720aea2ff7cd4d38dffed754
Rachel Lam <rachellamrlka@gmail.com>
2024-02-24 14:31:51 +0800

    progress on profile page

--- NTUtor/ProfilePage/ProfilePage.swift
+++ NTUtor/ProfilePage/ProfilePage.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+let darkBlue = Color(red: 53/255, green: 105/255, blue: 251/255)
+let lightBlue = Color(red: 45/255, green: 213/255, blue: 255/255)
+
 struct ProfilePage: View {
     var body: some View {
-        Text("Sup im rae nae")
+        VStack{
+            Text("Rachel Nae")
+                .offset(x:20, y:-330)
+                .bold()
+                .foregroundColor(darkBlue)
+            HStack{
+                Text("BCG")
+                    .italic()
+                Text("Year 1")
+            }.offset(x:20, y:-325)
+            
+        }
     }
 }
 

74b1876e8287ebd93c368eb3f3d04ac2b566e77e
Rachel Lam <rachellamrlka@gmail.com>
2024-02-24 12:59:43 +0800

    Create new profile page

---
+++ NTUtor/ProfilePage/ProfilePage.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePage.swift
+//  NTUtor
+//
+//  Created by rachel lam on 24/2/24.
+//
+
+import SwiftUI
+
+struct ProfilePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePage()
+}

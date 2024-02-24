c5cecd7f9d9c3812b4e018b002ff99c9617fdd4e
owennigel1 <owennigel1@gmail.com>
2024-02-24 15:36:19 +0800

    Adding bottombar view

---
+++ NTUtor/BottomBar/BottomBarView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomBarView.swift
+//  NTUtor
+//
+//  Created by Owen Nigel on 24/2/24.
+//
+
+import SwiftUI
+
+struct BottomBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomBarView()
+}

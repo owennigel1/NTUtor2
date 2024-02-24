7f4f5a854412123f371379e61f92cf7b397686d3
owennigel1 <owennigel1@gmail.com>
2024-02-24 16:14:29 +0800

    OIII DARRICK YOUR CONFIRMATION SWIFT CRASHED MY PROGRAM HAHAHA

--- NTUtor/SearchTutPage/Confirmation.swift
+++ NTUtor/SearchTutPage/Confirmation.swift
@@ -11,9 +11,9 @@ struct Confirmation: View {
     var body: some View {
         VStack{
             Text("BOOKED")
-                .font(.system(size: 30, weight = .bold))
+//                .font(.system(size: 30, weight = .bold))
                 .bold()
-            Text("A Confirmation Email Has Been Sent to \(tutorEmail)")
+//            Text("A Confirmation Email Has Been Sent to \(tutorEmail)")
         }
     }
 }

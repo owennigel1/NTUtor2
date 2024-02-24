3d6f4cbf33303f8480098cb4b9172a2e8d36a574
owennigel1 <owennigel1@gmail.com>
2024-02-24 10:24:44 +0800

    Initial Commit

---
+++ NTUtor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NTUtor
+//
+//  Created by Owen Nigel on 24/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NTUtor/NTUtorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTUtorApp.swift
+//  NTUtor
+//
+//  Created by Owen Nigel on 24/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NTUtorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

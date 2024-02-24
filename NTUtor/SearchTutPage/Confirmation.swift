4b64615d600334f2dc126217b94737841f65e6de
dcjh <140426836+dcjh@users.noreply.github.com>
2024-02-24 15:35:34 +0800

    booked page

---
+++ NTUtor/SearchTutPage/Confirmation.swift
@@ -0,0 +1,23 @@
+//
+//  Confirmation.swift
+//  NTUtor
+//
+//  Created by Darrick Chew on 24/2/24.
+//
+
+import SwiftUI
+
+struct Confirmation: View {
+    var body: some View {
+        VStack{
+            Text("BOOKED")
+                .font(.system(size: 30, weight = .bold))
+                .bold()
+            Text("A Confirmation Email Has Been Sent to \(tutorEmail)")
+        }
+    }
+}
+
+#Preview {
+    Confirmation()
+}

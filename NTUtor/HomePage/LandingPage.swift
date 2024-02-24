98e4824d01cd2d8228e786397499d61891aa510c
owennigel1 <owennigel1@gmail.com>
2024-02-24 14:57:24 +0800

    Add banner

--- NTUtor/HomePage/LandingPage.swift
+++ NTUtor/HomePage/LandingPage.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct LandingPage: View {
     var body: some View {
+        Image("banner")
+            .resizable()
+            .frame(width: 250, height: 60)
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Color("SecondaryColor")
     }
 }
 

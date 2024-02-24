c7452a6dc6edf0730144833f55459a7e2d58d7aa
dcjh <140426836+dcjh@users.noreply.github.com>
2024-02-24 11:01:55 +0800

    new swiftui

---
+++ NTUtor/New Group/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  NTUtor
+//
+//  Created by Darrick Chew on 24/2/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}

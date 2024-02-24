75d48d66b7234673e478c0952f148ba7e8066295
dcjh <140426836+dcjh@users.noreply.github.com>
2024-02-24 12:13:07 +0800

    created landing page

---
+++ NTUtor/HomePage/LandingPage.swift
@@ -0,0 +1,18 @@
+//
+//  LandingPage.swift
+//  NTUtor
+//
+//  Created by Darrick Chew on 24/2/24.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingPage()
+}

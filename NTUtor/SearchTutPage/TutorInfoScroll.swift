b974c0cfb327b5250f406efab685ef75cea2ca3a
owennigel1 <owennigel1@gmail.com>
2024-02-25 08:08:43 +0800

    separate model from view

--- NTUtor/SearchTutPage/TutorInfoScroll.swift
+++ NTUtor/SearchTutPage/TutorInfoScroll.swift
@@ -8,41 +8,7 @@
 
 import SwiftUI
 
-struct person: Identifiable {
-    var id = UUID()
-    var name: String
-    var tele: String
-    var course: String
-    var year: Int
-    var grade: String
-    var rate: String
-    var pic: String
-}
-
 struct TutorInfoScroll: View {
-    
-    var tutorInfo: [person]
-    let name = ["Owen", "John", "Michael", "Darrick", "Rachel", "Bob"]
-    let tele = ["owennigel",  "johnnysins", "m.angelo", "d_cjh",  "raeheehaw", "bobDeBuilder"]
-    let course = ["BCG", "CS", "SSM", "CSC", "BCG", "EEE"]
-    let year = [1, 2, 4, 1, 1, 3]
-    let grade = ["A+", "B+", "A-", "A+", "A+", "B-"]
-    let rate = ["$15/HR", "$5/HR", "$30/HR", "$20/HR", "$30/HR", "$40/HR"]
-    let pic = ["ProfilePic", "ProfilePic2", "ProfilePic3", "ProfilePic4", "ProfilePic5", "ProfilePic6"]
-
-    init() {
-        var tempInfo = [person]()
-        for i in 0..<name.count {
-            tempInfo.append(person(name: name[i],
-                                    tele: tele[i],
-                                    course: course[i],
-                                    year: year[i],
-                                    grade: grade[i],
-                                    rate: rate[i],
-                                    pic: pic[i]))
-        }
-        tutorInfo = tempInfo
-    }
     
     var body: some View {
         NavigationView {
@@ -84,7 +50,7 @@ struct TutorInfoScroll: View {
                 ZStack{
                     Color(.white)
                     ScrollView (showsIndicators : false){
-                        ForEach(tutorInfo) { info in
+                        ForEach(TutorData().tutorInfo) { info in
                             VStack(spacing: 20) {
                                 NavigationLink (destination: TutDescriptionPageView().navigationBarBackButtonHidden(true)) {
                                     DisplayTutors(name: info.name,

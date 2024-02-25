baf3add280b2d5e47d5e008531ad7012275cc3f3
owennigel1 <owennigel1@gmail.com>
2024-02-25 10:35:16 +0800

    Add marcoboy

--- NTUtor/TestData/TutorData.swift
+++ NTUtor/TestData/TutorData.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct TutorData {
     var tutorInfo: [person]
-    let name = ["Murtaza", "John", "Michael", "Darrick", "Rachel", "Bob"]
-    let tele = ["murtazalover",  "johnnysuns", "m.angelo", "d_cjh",  "raeheehaw", "bobDeBuilder"]
-    let course = ["EEE", "CS", "SSM", "CSC", "BCG", "EEE"]
-    let year = [1, 2, 4, 1, 1, 3]
-    let grade = ["A+", "B+", "A-", "A+", "A+", "B-"]
-    let rate = ["$35/HR", "$5/HR", "$30/HR", "$20/HR", "$30/HR", "$40/HR"]
-    let pic = ["MurtazaPic", "ProfilePic2", "ProfilePic3", "ProfilePic4", "ProfilePic5", "ProfilePic6"]
+    let name = ["Murtaza", "John", "Michael", "Darrick", "Rachel", "Bob", "Marco"]
+    let tele = ["murtazalover",  "johnnysuns", "m.angelo", "d_cjh",  "raeheehaw", "bobDeBuilder", "marcoriri"]
+    let course = ["EEE", "CSC", "SSM", "CSC", "BCG", "EEE", "CEE"]
+    let year = [1, 2, 4, 1, 1, 3, 2]
+    let grade = ["A+", "B+", "A-", "A+", "A+", "B-", "A+"]
+    let rate = ["$35/HR", "$5/HR", "$30/HR", "$20/HR", "$30/HR", "$40/HR", "$30/HR"]
+    let pic = ["MurtazaPic", "ProfilePic2", "ProfilePic3", "ProfilePic4", "RaeNae", "ProfilePic6","MarcoPic"]
     let bio = ["""
                Murtaza, a diligent and driven university student, brings a blend of intellectual curiosity and cultural richness to campus life.
                """,
@@ -30,6 +30,8 @@ Darrick, a charismatic presence on campus, combines effortless charm with a shar
 Rachel, a beacon of intellect and grace, captivates those around her with her quiet confidence and unwavering determination.
 """, """
 Bob, a laid-back and affable presence on campus, navigates university life with an easygoing charm. 
+""","""
+Marco, a dynamic and outgoing figure on campus, embodies the spirit of adventure and exploration. With a perpetual zest for life, he thrives in the university environment, seizing every opportunity to engage with new ideas and experiences.
 """]
     let date_available = [
         [
@@ -59,7 +61,11 @@ Bob, a laid-back and affable presence on campus, navigates university life with 
         "15 Jan 2024 | 16:00",
         "14 Jan 2024 | 12:00",
         "19 Jan 2024 | 09:00",
-    ]
+    ],
+        [
+        "11 Jan 2024 | 11:00",
+        "12 Jan 2024 | 13:00",
+    ],
     ]
 
     init() {

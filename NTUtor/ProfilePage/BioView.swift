834ba6c96b1485e56237ac536a637ef82abbc5be
owennigel1 <owennigel1@gmail.com>
2024-02-25 09:59:16 +0800

    Clear source control conflict

--- NTUtor/ProfilePage/BioView.swift
+++ NTUtor/ProfilePage/BioView.swift
@@ -8,20 +8,8 @@
 import SwiftUI
 
 struct BioView: View {
-<<<<<<< HEAD
-    var Bio: String = """
-    Hello! I am Owen, a Y1 Business &
-    Computer Science student. I provide
-    tutoring for the following mods on
-    Mon-Wed, 1-3pm and 8-10pm.
     
-    - SC1005 Digital Logic
-    - MH1812 Discrete Math
-    - AB1202 Statistics and Analysis
-    """
-=======
     var bio: String
->>>>>>> main
     
     var body: some View {
         ZStack{
